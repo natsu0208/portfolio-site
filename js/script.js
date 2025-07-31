4cb07770ce4e36ec75c9e1ec4e9b15251a44ac49
natsu0208 <59natsumi@gmail.com>
2025-07-31 15:46:50 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,95 @@
+$(function () {
+
+  //下からフェードイン
+$(window).on('load scroll',function (){
+		$('.fade').each(function(){
+			var target = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var height = $(window).height();
+			if (scroll > target - height){
+				$(this).addClass('active');
+			}
+		});
+	});
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+  //1文字ずつフェードイン
+  $('.eachTextAnime').each(function () {
+    var target = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var height = $(window).height();
+    if (scroll >= target - height) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+
+
+  
+//1文字ずつフェードイン
+    // eachTextAnimeにappeartextというクラス名を付ける定義
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var target = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var height = $(window).height();
+    if (scroll >= target - height) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  //spanタグを追加する
+  var element = $(".eachTextAnime");
+  element.each(function () {
+    var text = $(this).text();
+    var textbox = "";
+    text.split('').forEach(function (t, i) {
+      if (t !== " ") {
+        if (i < 10) {
+          textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+        } else {
+          var n = i / 10;
+          textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+        }
+
+      } else {
+        textbox += t;
+      }
+    });
+    $(this).html(textbox);
+  });
+
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+});
